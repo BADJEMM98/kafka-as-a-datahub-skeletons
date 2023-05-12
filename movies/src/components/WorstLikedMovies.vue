3e666fbef7210a2b54d5f9f0e5ecf90dac46a23f
boudjasma <asma.boudjatat@gmail.com>
2023-05-12 15:12:58 +0200

    rectification

--- movies/src/components/WorstLikedMovies.vue
+++ movies/src/components/WorstLikedMovies.vue
@@ -1,10 +1,6 @@
 <script>
-import MovieDetail from '/src/components/MovieDetail.vue'
 export default {
     name: 'MoviesList',
-    components: {
-        MovieDetail,
-    },
     props:{
     }
 }
@@ -14,26 +10,23 @@ export default {
     <div class="card text-center border-0 p-4 m-4">
         <table class="table table-dark table-hover table-responsive">
             <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Title</th>
-                <th scope="col">Nombre de vues</th>
-                <th scope="col">Stats</th>
-            </tr>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Score</th>
+                </tr>
             </thead>
             <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>
-                    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Afficher les détails
-                    </button>
-                    <MovieDetail title="titre" id="2" views="500" score="3.2"/>
-                </td>
-
-            </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>movie title 1</td>
+                    <td>2.0</td>
+                </tr>
+                <tr>
+                    <th scope="row">100</th>
+                    <td>movie title 3</td>
+                    <td>3</td>
+                </tr>
             </tbody>
         </table>
     </div>
